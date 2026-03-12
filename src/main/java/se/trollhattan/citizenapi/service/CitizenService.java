9b3b078817007179064fb65361e32a40efd06b8d
Raffi02k <Raffi9998@gmail.com>
2026-03-12 14:02:16 +0100

    Add basic fake lookup logic in citizen service

--- src/main/java/se/trollhattan/citizenapi/service/CitizenService.java
+++ src/main/java/se/trollhattan/citizenapi/service/CitizenService.java
@@ -7,6 +7,14 @@ import se.trollhattan.citizenapi.api.model.GuidResponse;
 public class CitizenService {
 
     public GuidResponse getGuid(String municipalityId, String personNumber) {
-        return new GuidResponse("test-party-id-123");
+        if ("1488".equals(municipalityId) && "199001011234".equals(personNumber)) {
+            return new GuidResponse("test-party-id-123");
+        }
+
+        if ("1488".equals(municipalityId) && "198512121212".equals(personNumber)) {
+            return new GuidResponse("test-party-id-456");
+        }
+
+        return new GuidResponse("unknown-party-id");
     }
 }
